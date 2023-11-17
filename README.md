8aa2c70854cf74b73af8f70d675bb9f4cd14b236
et120 <e.trotter120@gmail.com>
2023-11-17 10:47:43 -0800

    local storage + remove name from array

--- README.md
+++ README.md
@@ -14,4 +14,6 @@ Random Name Generator
         return our name
     Set text tags to our random name
 
-3. Remove a name
+3. Remove a name: button
+    Take in name input and get its index within our name array
+    use .splice to remove that element
--- scripts/app.js
+++ scripts/app.js
@@ -3,16 +3,30 @@ import { RandomName } from "./randomfunction.js";
 let nameInput = document.getElementById("nameInput");
 let addBtn = document.getElementById("addBtn");
 let randomBtn = document.getElementById("randomBtn");
+let nameTxt = document.getElementById("nameTxt");
+let removeBtn = document.getElementById("removeBtn");
 
 let nameArray = [];
+if(localStorage.getItem("names")){
+    nameArray = JSON.parse(localStorage.getItem("names"))
+};
 
 addBtn.addEventListener('click', function(){
     nameArray.push(nameInput.value);
     nameInput.value = "";
     console.log(nameArray);
+
+    localStorage.setItem("names", JSON.stringify(nameArray));
 });
 
 randomBtn.addEventListener('click', function(){
-    RandomName(nameArray);
+    nameTxt.innerText = RandomName(nameArray);
 });
 
+removeBtn.addEventListener('click', function(){
+    let index = nameArray.indexOf(nameInput.value);
+    nameArray.splice(index, 1);
+    nameInput.value = "";
+
+    localStorage.setItem("names", JSON.stringify(nameArray));
+});
